9c88603d55dedc844599e5e4268ad91a9a8d8e2e
neymarsabin <reddevil.sabin@gmail.com>
2025-04-14 15:16:31 +0545

    zipped

--- q4.m
+++ q4.m
@@ -3,20 +3,20 @@ coffee_fuzzy = mamfis('Name', 'CoffeeStrength');
 
 %% Variable: Bean Type (X1)
 coffee_fuzzy = addInput(coffee_fuzzy, [0 8], 'Name', 'Bean_Type');
-coffee_fuzzy = addMF(coffee_fuzzy, 'Bean_Type', 'trapmf', [0 0 1 2], 'Name', 'Pahadi_Light');
-coffee_fuzzy = addMF(coffee_fuzzy, 'Bean_Type', 'trapmf', [2 3 3 4], 'Name', 'Himalayan_Medium');
-coffee_fuzzy = addMF(coffee_fuzzy, 'Bean_Type', 'trapmf', [4 5 5 6], 'Name', 'Everest_Bold');
-coffee_fuzzy = addMF(coffee_fuzzy, 'Bean_Type', 'trapmf', [6 7 8 8], 'Name', 'Kanchanjunga_Dark');
+coffee_fuzzy = addMF(coffee_fuzzy, 'Bean_Type', 'trapmf', [0 0 1 3], 'Name', 'Pahadi_Light');
+coffee_fuzzy = addMF(coffee_fuzzy, 'Bean_Type', 'trapmf', [1 2 4 5], 'Name', 'Himalayan_Medium');
+coffee_fuzzy = addMF(coffee_fuzzy, 'Bean_Type', 'trapmf', [3 4 6 7], 'Name', 'Everest_Bold');
+coffee_fuzzy = addMF(coffee_fuzzy, 'Bean_Type', 'trapmf', [5 6 8 8], 'Name', 'Kanchanjunga_Dark');
 figure;
 plotmf(coffee_fuzzy, 'input', 1);
 title('Membership Functions for Bean Type');
 
 %% Variable: Brewing Duration (X2)
 coffee_fuzzy = addInput(coffee_fuzzy, [0 10], 'Name', 'Brewing_Duration');
-coffee_fuzzy = addMF(coffee_fuzzy, 'Brewing_Duration', 'trapmf', [0 0 1 3], 'Name', 'Short');
-coffee_fuzzy = addMF(coffee_fuzzy, 'Brewing_Duration', 'trapmf', [2 3 5 6], 'Name', 'Medium');
+coffee_fuzzy = addMF(coffee_fuzzy, 'Brewing_Duration', 'trapmf', [0 0 2 4], 'Name', 'Short');
+coffee_fuzzy = addMF(coffee_fuzzy, 'Brewing_Duration', 'trapmf', [2 3 6 7], 'Name', 'Medium');
 coffee_fuzzy = addMF(coffee_fuzzy, 'Brewing_Duration', 'trapmf', [5 6 8 9], 'Name', 'Long');
-coffee_fuzzy = addMF(coffee_fuzzy, 'Brewing_Duration', 'trapmf', [8 9 10 10], 'Name', 'Very_Long');
+coffee_fuzzy = addMF(coffee_fuzzy, 'Brewing_Duration', 'trapmf', [7 8 10 10], 'Name', 'Very_Long');
 figure;
 plotmf(coffee_fuzzy, 'input', 2);
 title('Membership Functions for Brewing Duration');
@@ -24,8 +24,8 @@ title('Membership Functions for Brewing Duration');
 %% Input Variable: Water Temperature (X3)
 coffee_fuzzy = addInput(coffee_fuzzy, [50 110], 'Name', 'Water_Temperature');
 coffee_fuzzy = addMF(coffee_fuzzy, 'Water_Temperature', 'trapmf', [50 50 60 70], 'Name', 'Low');
-coffee_fuzzy = addMF(coffee_fuzzy, 'Water_Temperature', 'trapmf', [70 75 85 90], 'Name', 'Medium');
-coffee_fuzzy = addMF(coffee_fuzzy, 'Water_Temperature', 'trapmf', [90 95 110 110], 'Name', 'High');
+coffee_fuzzy = addMF(coffee_fuzzy, 'Water_Temperature', 'trapmf', [60 75 85 90], 'Name', 'Medium');
+coffee_fuzzy = addMF(coffee_fuzzy, 'Water_Temperature', 'trapmf', [80 95 110 110], 'Name', 'High');
 figure;
 plotmf(coffee_fuzzy, 'input', 3);
 title('Membership Functions for Water Temperature');
